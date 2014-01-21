1b510674d87acf44c07f21611c89a4b78be7eef7
Jonas Almeida <jalmeida@mathbiol.org>
2014-01-21 16:13:19 -0600

    pinging arbitrary mongodb backend

---
+++ ping.js
@@ -0,0 +1,23 @@
+﻿var mongodb = require("mongodb");
+var http = require("http");
+var corser = require("corser"); // Thank you Alex - this is great :-)!!!
+var log = console.log;
+var port = process.env.PORT || 1337;
+log('POSTmongo served at port '+port);
+
+// Create Corser request listener.
+var corserRequestListener = corser.create();
+
+http.createServer(function (req, res) {
+    // Route req and res through the request listener.
+    corserRequestListener(req, res, function () {
+        if (req.method === "OPTIONS") {
+            // End CORS preflight request.
+            res.writeHead(204);
+            res.end();
+        } else {
+            res.writeHead(200);
+            res.end(':-)');
+        }
+    });
+}).listen(port);
